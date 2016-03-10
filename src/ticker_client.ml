db5885d4f4554c583358cb5c232f17b063f8f864
manu <manu.delab@gmail.com>
2016-03-10 20:57:55 +1300

    initial commit

--- src/ticker_client.ml
+++ src/ticker_client.ml
@@ -1,38 +1,37 @@
 open Lwt.Infix
-open Cohttp_lwt_unix
+open Websocket_lwt
+
+let request uri =
+  let open Frame in
+  Resolver_lwt.resolve_uri ~uri Resolver_lwt_unix.system >>= fun endp ->
+  Conduit_lwt_unix.(endp_to_client ~ctx:default_ctx endp >>= fun client ->
+  with_connection ~ctx:default_ctx client uri) >>= fun (recv, send) ->
+  let rec handle_frame fr =
+    let open Opcode in
+    match fr.opcode with
+    | Ping -> send @@ Frame.create ~opcode:Pong ()
+    | Pong -> Lwt.return_unit
+    | Binary 
+    | Text -> print_frame fr
+    | Close
+    | _ -> send @@ Frame.close 1002 >> Lwt.fail Exit
+  and print_frame fr =
+    Yojson.Basic.from_string fr.content
+    |> Yojson.Basic.pretty_to_string
+    |> Lwt_io.printl 
+  in
+  let rec loop () = 
+    send @@ Frame.create ~opcode:Opcode.Text ~content:"tick" () >> 
+    recv () >>= handle_frame >> Lwt.return (Unix.sleep 1) >>= loop
+  in loop ()
 
 
-let rec request () =
-  (* Some bogus ask amd bid price *)
-  Client.get (Uri.of_string "http://127.0.0.1:8000/ticker")
-  (** Bind, or as some affectionally call shove: 'a Lwt.t -> (a' -> b'
-      Lwt.t) -> 'b Lwt.t What we're doing here is shoving the thing inside
-      the monad on the left of the >>= into the right side of the >>= then
-      wrapping that result in a monad.  Another way to think about this
-      might be: "I don't know when the left side will happen, but when it
-      does, do this with it." *)
-  >>= fun (resp, body) -> (
-  (** Some debugging code for you to play with, technically bad since
-      its using plain printf and not Lwt's Lwt_io.print functions *)
-  (* let code = resp |> Response.status |> Cohttp.Code.code_of_status in *)
-  (* Printf.printf "Response code: %d\n" code; *)
-  (* Printf.printf "Headers: %s\n" (resp |> Response.headers |> Cohttp.Header.to_string); *)
-  body |> Cohttp_lwt_body.to_string >|= (fun as_str ->
-  Yojson.Basic.from_string as_str
-  |> Yojson.Basic.pretty_to_string
-  |> Lwt_io.printl ))
+let () = 
+  Lwt_main.run (request @@ Uri.of_string "http://127.0.0.1:8000/ticker") 
+  |> Lwt.ignore_result
 
-let () =
-  (** Lwt is a cooperative threading library in OCaml. Cooperative
-      threads (A.K.A. non-premtive threads) typically yield control to
-      another thread before they finish.  People use Lwt for I/O, events,
-      and ease of use. See also, async. *)
 
-  (* Lwt_main.run : 'a Lwt.t -> 'a *)
-  (** run t calls the Lwt scheduler repeatedly until t terminates,
-      then returns the value returned by the thread.  If t fails with
-      an exception the exception is raised. *)
-  Lwt_main.run (request ()) |> Lwt.ignore_result
+
 
 
 
